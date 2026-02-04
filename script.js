8f7c6a0c46ff84fdaf0fb049996febb613e2521f
Josh Guin <131725138+jguin8@users.noreply.github.com>
2026-02-04 13:43:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const MASTER_MAP = [
                 "groupName": "ERP Systems",
                 "items":[
                     {"name": "Terra Rose", "shortcutKey": "t", "url": "https://6751627.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
+                    {"name": "Terra Rose Oracle Cloud, "shortcutKey": "t", "url": "https://idcs-d1c722842bbc49d58e9c746cb2bfaa96.identity.oraclecloud.com/ui/v1/signin"},
                     //{"name": "Everyone Designs", "shortcutKey": "e", "url": "https://7236299.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
                     //{"name": "House of Outdoors", "shortcutKey": "h", "url": "https://7576050.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
                     {"name": "Great Star SAP", "shortcutKey": "h", "url": "https://my433127.s4hana.cloud.sap/ui"},
